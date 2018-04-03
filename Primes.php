70fd25b2e66ac0a499139a6dcf80579ccbbbe934
hanad <cabdiqani_hanad@outlook.com>
2018-04-03 10:45:35 +0400

    Add files via upload

---
+++ Primes.php
@@ -0,0 +1,17 @@
+<?php
+
+for ($i=3; $i < 1000; $i++) { 
+	$prime="yes";
+	for ($y=2; $y < $x; $y++) { 
+		if ($x%$y==0) {
+			$prime="no"
+			if ($prime=="yes") echo echo $x. '' {
+				# code...
+			}
+		}
+	}
+}
+
+
+
+?>
